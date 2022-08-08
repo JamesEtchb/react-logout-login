fc3fd6f017474ad7c33cf86c50393aff6a1069b9
james etchberger <etchbergerjames@gmail.com>
2022-08-08 16:54:13 -0400

    added some more styling and other touches

--- src/components/Coffee.jsx
+++ src/components/Coffee.jsx
@@ -2,28 +2,45 @@ import { useState } from 'react'
 
 export default function Coffee() {
   const [coffeeList, setCoffeeList] = useState()
-  const getCoffee = (temperature) => {
-    fetch(`https://api.sampleapis.com/coffee/${temperature}`)
-        .then(results => results.json())
-        .then(data => setCoffeeList(data))
-        .catch(err => alert(err))
-    
+
+//this is the same as the async await method below but using .then
+//   const getCoffee = (temperature) => {
+//     fetch(`https://api.sampleapis.com/coffee/${temperature}`)
+//       .then((results) => results.json())
+//       .then((data) => setCoffeeList(data))
+//       .catch((err) => alert(err))
+//   }
+
+  const getCoffeeSync = async (temperature) => {
+    try {
+        const results = await fetch(`https://api.sampleapis.com/coffee/${temperature}`)
+        const data = await results.json()
+        setCoffeeList(data)
+    }
+    catch(err){
+        alert(err)
+    }
   }
+
   return (
     <>
-      <button onClick={() => getCoffee('hot')}>HOT</button>
-      <button onClick={() => getCoffee('iced')}>ICED</button>
+      <button onClick={() => getCoffeeSync('hot')}>HOT</button>
+      <button onClick={() => getCoffeeSync('iced')}>ICED</button>
       <section>
         <h2>Coffee List</h2>
-        {!coffeeList 
-        ? <p>Loading...</p>
-        : coffeeList.map(coffee => (
+        {!coffeeList ? (
+          <p>Loading...</p>
+        ) : (
+          coffeeList.map((coffee) => (
             <ui>
-            <ui key={coffee.id}><b>{coffee.title}</b></ui>
-            <li key={coffee.id}>{coffee.description}</li>
+              <ui key={coffee.id}>
+                <b><i>{coffee.title}</i></b>
+              </ui><br />
+              <li key={coffee.id}>{coffee.description}</li>
+              <br />
             </ui>
-        ))
-        }
+          ))
+        )}
       </section>
     </>
   )
