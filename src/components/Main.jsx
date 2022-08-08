b73eecd7c94e844467ef0e0c6ee1778496c1a88a
james etchberger <etchbergerjames@gmail.com>
2022-08-08 15:16:12 -0400

    if you sign out you cant see the info and it says guest

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,17 @@
+import StudentCard from './StudentCard'
+
+const student = {
+  name: 'Khali Gopaul',
+  age: 37,
+  id: 'r64j83',
+  term: 'Su22',
+}
+
 export default function Main({ name }) {
   return (
     <main>
-      <h1>Hello {name}</h1>
+      <h1>Hello {name || 'Guest'}</h1> 
+      {name && <StudentCard student={student} />}
     </main>
   )
 }
--- src/components/Menubar.jsx
+++ src/components/Menubar.jsx
@@ -1,9 +1,11 @@
-export default function Menubar () {
-    return (
-        <nav>
-            Menubar
-        </nav>
-    )
+export default function Menubar({ name, setName }) {
+  return (
+    <nav style={{ display: 'flex', justifyContent: 'space-between' }}>
+      <span>Menubar</span>
+      {name 
+        ? <button onClick={() => setName(null)}>Logout</button> 
+        : <button onClick={() => setName('James')}>Login</button>
+      }
+    </nav>
+  )
 }
-
-
